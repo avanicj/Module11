611dc649e7f12a8482ae8f9734bd52edee2480a0
avanicj <avaniaj15@gmail.com>
2025-04-17 09:33:47 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,90 @@
+import React from "react";
+
+class IssueRow extends React.Component {
+  render() {
+    const issue = this.props.issue;
+    return (
+      <tr>
+        <td>{issue.id}</td>
+        <td>{issue.status}</td>
+        <td>{issue.owner}</td>
+        <td>{issue.created.toDateString()}</td>
+        <td>{issue.effort}</td>
+        <td>{issue.completionDate ? issue.completionDate.toDateString() : ""}</td>
+        <td>{issue.title}</td>
+      </tr>
+    );
+  }
+}
+
+class IssueTable extends React.Component {
+  render() {
+    const issueRows = this.props.issues.map(issue => (
+      <IssueRow key={issue.id} issue={issue} />
+    ));
+    const borderedStyle = { border: "1px solid silver", padding: 6 };
+    return (
+      <table style={{ borderCollapse: "collapse" }}>
+        <thead>
+          <tr>
+            <th style={borderedStyle}>Id</th>
+            <th style={borderedStyle}>Title</th>
+          </tr>
+        </thead>
+        <tbody>{issueRows}</tbody>
+      </table>
+    );
+  }
+}
+
+class IssueFilter extends React.Component {
+  render() {
+    return <div>This is a placeholder for the Issue Filter.</div>;
+  }
+}
+
+class IssueAdd extends React.Component {
+  render() {
+    return <div>This is a placeholder for an Issue Add entry form.</div>;
+  }
+}
+
+const issues = [
+  {
+    id: 1,
+    status: "Open",
+    owner: "Ravan",
+    created: new Date("2016-08-15"),
+    effort: 5,
+    completionDate: undefined,
+    title: "Error in console when clicking Add"
+  },
+  {
+    id: 2,
+    status: "Assigned",
+    owner: "Eddie",
+    created: new Date("2016-08-16"),
+    effort: 14,
+    completionDate: new Date("2016-08-30"),
+    title: "Missing bottom border on panel"
+  }
+];
+
+class IssueList extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Issue Tracker</h1>
+        <IssueFilter />
+        <hr />
+        <IssueTable issues={issues} />
+        <hr />
+        <IssueAdd />
+      </div>
+    );
+  }
+}
+
+export default function App() {
+  return <IssueList />;
+}
